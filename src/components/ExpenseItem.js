f8fa9fd3b49ef98af1ce68430cf28f3b210b284c
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-10 00:59:27 +0900

    ✨ : ExpenseItem 컴포넌트 추가

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class ExpenseItem extends Component {
   render() {
     return (
-      <div>ExpenseItem</div>
-    )
+      <li className="item">
+        <div className="info">
+          <span className="expense"></span>
+          <span className="amount"> 원</span>
+        </div>
+        <div>
+          <button className="edit-btn">수정</button>
+          <button className="dlear-btn">삭제</button>
+        </div>
+      </li>
+    );
   }
 }
 
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import "./ExpenseList.css"
+import React, { Component } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpenseList.css";
+
 export class ExpenseList extends Component {
   render() {
     return (
       <>
-        <ul className='list'>
+        <ul className="list">
           {/* Expense Item */}
+          <ExpenseItem />
         </ul>
-        <button  className='btn'>
-          목록 지우기
-        </button>
+        <button className="btn">목록 지우기</button>
       </>
-    )
+    );
   }
 }
 
-export default ExpenseList
+export default ExpenseList;
