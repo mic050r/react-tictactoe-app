fa4cc9dbb3e66ce6bb459c53ef922a5e16f93c03
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-29 23:29:55 +0900

    ✨ : 지출 항목 수정 기능 구현

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 import { MdDelete } from "react-icons/md";
 
-const ExpenseList = ({ handleDelete, initialExpense }) => {
+const ExpenseList = ({ handleDelete, initialExpense, handleEdit }) => {
   return (
     <>
       <ul className="list">
@@ -14,6 +14,7 @@ const ExpenseList = ({ handleDelete, initialExpense }) => {
               expense={expense}
               key={expense.id}
               handleDelete={handleDelete}
+              handleEdit={handleEdit}
             />
           );
         })}
