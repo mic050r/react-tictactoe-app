09bfa1b3f99974cf8f0d9addcec5e0817af29142
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-29 18:49:20 +0900

    ♻️ : 리액트 Hooks를 이용해서 예산 계산기 앱을 함수형 컴포넌트로 바꾸기

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ExpenseForm.css";
 import { MdSend } from "react-icons/md";
-export class ExpenseForm extends Component {
-  render() {
-    return (
-      <form>
-        {/* 지출항목 부분 */}
-        <div className="form-center">
-          <div className="form-group">
-            <label htmlFor="expense">지출 항목</label>
-            <input
-              type="text"
-              className="form-control"
-              id="charge"
-              name="charge"
-              placeholder="예) 렌트비"
-            />
-          </div>
-          {/* 비용 부분 */}
-          <div className="form-group">
-            <label htmlFor="amout">비용</label>
-            <input
-              type="number"
-              className="form-control"
-              id="amout"
-              name="amout"
-              placeholder="예) 1000원"
-            />
-          </div>
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      {/* 지출항목 부분 */}
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="expense">지출 항목</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="예) 렌트비"
+          />
         </div>
-        {/* 제출 버튼 */}
-        <button type="submit" className="btn">
-          제출
-          <MdSend className="btn-icon" />
-        </button>
-      </form>
-    );
-  }
-}
+        {/* 비용 부분 */}
+        <div className="form-group">
+          <label htmlFor="amout">비용</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amout"
+            name="amout"
+            placeholder="예) 1000원"
+          />
+        </div>
+      </div>
+      {/* 제출 버튼 */}
+      <button type="submit" className="btn">
+        제출
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
+};
 
 export default ExpenseForm;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 import { MdDelete } from "react-icons/md";
 
-export class ExpenseList extends Component {
-  render() {
-    console.log(this.props.initialExpense);
-    return (
-      <>
-        <ul className="list">
-          {/* Expense Item */}
-          {this.props.initialExpense.map((expense) => {
-            return (
-              <ExpenseItem
-                expense={expense}
-                key={expense.id}
-                handleDelete={this.props.handleDelete}
-              />
-            );
-          })}
-        </ul>
-        <button className="btn">
-          목록 지우기
-          <MdDelete className="btn-icon" />
-        </button>
-      </>
-    );
-  }
-}
+const ExpenseList = ({ handleDelete, initialExpense }) => {
+  return (
+    <>
+      <ul className="list">
+        {/* Expense Item */}
+        {initialExpense.map((expense) => {
+          return (
+            <ExpenseItem
+              expense={expense}
+              key={expense.id}
+              handleDelete={handleDelete}
+            />
+          );
+        })}
+      </ul>
+      <button className="btn">
+        목록 지우기
+        <MdDelete className="btn-icon" />
+      </button>
+    </>
+  );
+};
 
 export default ExpenseList;
